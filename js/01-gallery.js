6b45bfdcce837f152e5288fd5dd05b61bb5aff45
Igorippon <botvunaigor@gmail.com>
2023-06-22 22:09:51 +0300

    add.task.01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+function createMarkupitems(arr) {
+    return arr.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+     <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+      </a>
+      </li>`
+    ).join('');
+    
+}
+
+list.insertAdjacentHTML("beforeend", createMarkupitems(galleryItems));
+list.addEventListener('click', handlerClickImg);
+
+function handlerClickImg(evt) {
+   evt.preventDefault();
+
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    };
+
+    const imgItem = evt.target;
+    const obj = findImgItem(imgItem);
+
+    const instance = basicLightbox.create(createMarkupModal(obj));
+    instance.show();
+    
+}
+
+function findImgItem(item) {
+     const  original  = item.dataset.source;
+     const currentImg = galleryItems.find(({ original: imgoriginal }) => imgoriginal === original);
+     return currentImg
+    
+}
+
+function createMarkupModal({preview, original, description} = {}) {
+     return `<img
+      src="${original}"
+      data-source="${preview}"
+      alt="${description}"
+      />` 
+}
